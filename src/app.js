a13331589e513a965eab0b270eb3a62261d67e59
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-03-07 22:30:39 -0500

    Quick text styling updtate

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-let cliBlockWelcome = document.getElementById("cliBlockWelcome");
-let cliWelcome = document.getElementById("cliWelcome");
-let editorTouchBlock = document.getElementById("intentionBlockTouch");
-let editorTouch = document.getElementById("intTouch");
-let editorTimeBlock = document.getElementById("intentionBlockTime");
-let editorTime = document.getElementById("intTime");
+let cliBlockWelcome = document.getElementById('cliBlockWelcome');
+let cliWelcome = document.getElementById('cliWelcome');
+let editorTouchBlock = document.getElementById('intentionBlockTouch');
+let editorTouch = document.getElementById('intTouch');
+let editorTimeBlock = document.getElementById('intentionBlockTime');
+let editorTime = document.getElementById('intTime');
 let truthInput = document.querySelector('input[name="truth"]');
 let touchInput = document.querySelector('input[name="touch"]');
 let timeInput = document.querySelector('input[name="time"]');
@@ -15,53 +15,53 @@ cliWelcome.focus();
 // Adding event listeners
 
 function addInputEventListener(elem) {
-  elem.addEventListener("input", function () {
+  elem.addEventListener('input', function () {
     let editorID = this.id;
     let goalEditor = document.getElementById(editorID);
     let goal = goalEditor.innerText;
     console.log(goal);
     if (artisanWords.some((word) => goal.toLowerCase().includes(word))) {
-      templateExpSelector.style.backgroundColor = "#D3869B";
+      templateExpSelector.style.backgroundColor = '#D3869B';
     } else {
-      templateExpSelector.style.backgroundColor = "#8EC07C";
+      templateExpSelector.style.backgroundColor = '#8EC07C';
     }
     if (sentimentalistWords.some((word) => goal.toLowerCase().includes(word))) {
-      block2Selector.style.backgroundColor = "#FABD2F";
+      block2Selector.style.backgroundColor = '#FABD2F';
     } else {
-      block2Selector.style.backgroundColor = "#8EC07C";
+      block2Selector.style.backgroundColor = '#8EC07C';
     }
     if (adventurerWords.some((word) => goal.toLowerCase().includes(word))) {
-      block3Selector.style.backgroundColor = "#FD6D5C";
+      block3Selector.style.backgroundColor = '#FD6D5C';
     } else {
-      block3Selector.style.backgroundColor = "#8EC07C";
+      block3Selector.style.backgroundColor = '#8EC07C';
     }
   });
 }
 
 function revealCommands(elem) {
-  elem.addEventListener("input", function () {
+  elem.addEventListener('input', function () {
     let editorID = this.id;
     let goalEditor = document.getElementById(editorID);
     let goal = goalEditor.innerText;
     console.log(goal);
-    if (goal === "/") {
-      selectorGroup.style.display = "flex";
+    if (goal === '/') {
+      selectorGroup.style.display = 'flex';
     } else {
-      selectorGroup.style.display = "none";
+      selectorGroup.style.display = 'none';
     }
   });
 }
 
 function listenForEnter(elem) {
-  elem.addEventListener("keydown", function (e) {
+  elem.addEventListener('keydown', function (e) {
     let editorID = this.id;
     let goalEditor = document.getElementById(editorID);
     if (e.keyCode === 13) {
       e.preventDefault();
-      editorTouchBlock.classList.toggle("hidden");
+      editorTouchBlock.classList.toggle('hidden');
       editorTouch.focus();
-      cliBlockWelcome.classList.toggle("hidden");
-      selectorGroup.classList.toggle("hidden");
+      cliBlockWelcome.classList.toggle('hidden');
+      selectorGroup.classList.toggle('hidden');
     } else {
       return;
     }
@@ -69,23 +69,23 @@ function listenForEnter(elem) {
 }
 
 function hidePlaceholder(elem) {
-  elem.addEventListener("focus", function () {
+  elem.addEventListener('focus', function () {
     let editorID = this.id;
     let goalEditor = document.getElementById(editorID);
     if (goalEditor.innerText == experiencePlaceholder) {
-      goalEditor.innerText = "";
-      goalEditor.style.color = "#E38A57";
+      goalEditor.innerText = '';
+      goalEditor.style.color = '#E38A57';
     }
   });
 }
 
 function showPlaceholder(elem) {
-  elem.addEventListener("blur", function () {
+  elem.addEventListener('blur', function () {
     let editorID = this.id;
     let goalEditor = document.getElementById(editorID);
-    if (goalEditor.innerText == "") {
+    if (goalEditor.innerText == '') {
       goalEditor.innerText = experiencePlaceholder;
-      goalEditor.style.color = "grey";
+      goalEditor.style.color = 'grey';
     }
   });
 }
@@ -97,65 +97,65 @@ addInputEventListener(cliWelcome);
 
 // Goal placeholders
 let experiencePlaceholder =
-  "I want to… swim in the Pacific… see Springsteen in concert with my daughter… try chocolate-covered chapulines in Mexico City… read the works of Shakespeare in their original text…";
+  'I want to… swim in the Pacific… see Springsteen in concert with my daughter… try chocolate-covered chapulines in Mexico City… read the works of Shakespeare in their original text…';
 
 // Building the archetype string
-let archetypeString = "";
+let archetypeString = '';
 
 const artisanWords = [
-  "work",
-  "create",
-  "build",
-  "make",
-  "finish",
-  "develop",
-  "produce",
-  "engineer",
-  "craft",
+  'work',
+  'create',
+  'build',
+  'make',
+  'finish',
+  'develop',
+  'produce',
+  'engineer',
+  'craft',
 ];
 const sentimentalistWords = [
-  "love",
-  "loved",
-  "care",
-  "help",
-  "support",
-  "serve",
-  "nurture",
-  "teach",
-  "mentor",
-  "coach",
-  "counsel",
-  "guide",
-  "protect",
-  "family",
-  "friend",
-  "community",
-  "money",
+  'love',
+  'loved',
+  'care',
+  'help',
+  'support',
+  'serve',
+  'nurture',
+  'teach',
+  'mentor',
+  'coach',
+  'counsel',
+  'guide',
+  'protect',
+  'family',
+  'friend',
+  'community',
+  'money',
 ];
 const adventurerWords = [
-  "explore",
-  "travel",
-  "visit",
-  "hike",
-  "world",
-  "mountain",
-  "fitness",
-  "workout",
-  "sports",
-  "play",
-  "taste",
-  "experience",
+  'explore',
+  'travel',
+  'visit',
+  'hike',
+  'world',
+  'mountain',
+  'fitness',
+  'workout',
+  'sports',
+  'play',
+  'taste',
+  'experience',
 ];
 
-editorTime.addEventListener("input", function () {
+editorTime.addEventListener('input', function () {
   let timeText = editorTime.innerText;
   console.log(timeText);
   if (artisanWords.some((word) => timeText.toLowerCase().includes(word))) {
-    document.getElementById("templateBlock3Selector").style.backgroundColor =
-      "green";
+    document.getElementById('templateBlock3Selector').style.backgroundColor =
+      'green';
   } else {
-    document.getElementById("templateBlock3Selector").style.backgroundColor =
-      "red";
+    document.getElementById('templateBlock3Selector').style.backgroundColor =
+      'red';
   }
 });
 
@@ -168,49 +168,49 @@ let templateCounter = 0;
 function convertCounter(n) {
   n = String(templateCounter);
   if (n.length < 2) {
-    n = "0" + n;
+    n = '0' + n;
   }
   return n;
 }
 
-let parentSection = document.getElementById("content");
-let selectorGroup = document.getElementById("selectorGroup");
+let parentSection = document.getElementById('content');
+let selectorGroup = document.getElementById('selectorGroup');
 
-let templateExp = document.getElementById("experienceTemplate");
-let templateExpSelector = document.getElementById("templateExpSelector");
+let templateExp = document.getElementById('experienceTemplate');
+let templateExpSelector = document.getElementById('templateExpSelector');
 
-let block2 = document.getElementById("templateBlock2");
-let block2Selector = document.getElementById("templateBlock2Selector");
-let block3 = document.getElementById("templateBlock3");
-let block3Selector = document.getElementById("templateBlock3Selector");
+let block2 = document.getElementById('templateBlock2');
+let block2Selector = document.getElementById('templateBlock2Selector');
+let block3 = document.getElementById('templateBlock3');
+let block3Selector = document.getElementById('templateBlock3Selector');
 
-templateExpSelector.addEventListener("click", function () {
+templateExpSelector.addEventListener('click', function () {
   let newTemplate = templateExp.cloneNode(true);
   let newTemplateHeader = newTemplate.children[0];
   let newTemplateSubheader = newTemplate.children[1];
   let newTemplateExample = newTemplateSubheader.children[1];
   let newID = convertCounter(templateCounter);
-  newTemplate.id = "experienceTemplate" + newID;
-  newTemplateHeader.id = "experienceTemplateHeader" + newID;
-  newTemplateExample.id = "experienceTemplateExample" + newID;
+  newTemplate.id = 'experienceTemplate' + newID;
+  newTemplateHeader.id = 'experienceTemplateHeader' + newID;
+  newTemplateExample.id = 'experienceTemplateExample' + newID;
   parentSection.insertBefore(newTemplate, selectorGroup);
-  newTemplate.classList.toggle("hidden");
+  newTemplate.classList.toggle('hidden');
   addInputEventListener(newTemplateExample);
   hidePlaceholder(newTemplateExample);
   showPlaceholder(newTemplateExample);
   revealCommands(newTemplateExample);
   templateCounter++;
-  selectorGroup.style.display = "none";
+  selectorGroup.style.display = 'none';
 });
 
 // ------------------------------
 
-let viewConsiderations = document.querySelector(".considerations-toggle");
-let considerations = document.getElementById("considerationsPanel");
+let viewConsiderations = document.querySelector('.considerations-toggle');
+let considerations = document.getElementById('considerationsPanel');
 
-viewConsiderations.addEventListener("click", function () {
-  considerations.classList.toggle("hidden");
-  console.log("considerations toggled");
+viewConsiderations.addEventListener('click', function () {
+  considerations.classList.toggle('hidden');
+  console.log('considerations toggled');
 });
 
 // ------------------------------
