0452ff6bf18ac88f3c1267c44acae753c9ff842b
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-04-07 14:02:27 -0400

    Start laying out command flow logic and color commands

--- src/app.js
+++ src/app.js
@@ -1,57 +1,109 @@
-let cliBlockWelcome = document.getElementById('cliBlockWelcome');
-let cliWelcome = document.getElementById('cliWelcome');
+let cliBlock = document.getElementById('cliBlock');
+let prompt = document.getElementById('cliPrompt');
+let cliEditor = document.getElementById('cliEditor');
 let cliBlockHelp = document.getElementById('cliBlockHelp');
 let cliHelp = document.getElementById('cliHelp');
 let cliBlockDemo = document.getElementById('cliBlockDemo');
 let cliDemo = document.getElementById('cliDemo');
-let editorTimeBlock = document.getElementById('intentionBlockTime');
-let editorTime = document.getElementById('intTime');
-let truthInput = document.querySelector('input[name="truth"]');
-let touchInput = document.querySelector('input[name="touch"]');
-let timeInput = document.querySelector('input[name="time"]');
 
 // command booleans
 
-const commands = {
-  help: false,
-  clear: false,
-  achieve: false,
-  explore: false,
-  innovate: false,
-  connect: false,
-  fulfill: false,
-};
+const commands = [
+  {
+    name: 'help',
+    active: false,
+    color: '#FD6D5C',
+  },
+  {
+    name: 'achieve',
+    active: false,
+    color: '#D3869B',
+  },
+  {
+    name: 'explore',
+    active: false,
+    color: '#B8BB26',
+  },
+  {
+    name: 'innovate',
+    active: false,
+    color: '#83A598',
+  },
+  {
+    name: 'connect',
+    active: false,
+    color: '#8EC07C',
+  },
+  {
+    name: 'fulfill',
+    active: false,
+    color: '#FABD2F',
+  },
+];
 
-// focus on truth editor at start
+// focus on CLI editor at start
 
-cliWelcome.focus();
+cliEditor.focus();
 
 // Adding event listeners
 
-function addInputEventListener(elem) {
+// function addInputEventListener(elem) {
+//   elem.addEventListener('input', function () {
+//     let editorID = this.id;
+//     let goalEditor = document.getElementById(editorID);
+//     let goal = goalEditor.innerText;
+//     console.log(goal);
+//     if (artisanWords.some((word) => goal.toLowerCase().includes(word))) {
+//       templateExpSelector.style.backgroundColor = '#D3869B';
+//     } else {
+//       templateExpSelector.style.backgroundColor = '#8EC07C';
+//     }
+//     if (sentimentalistWords.some((word) => goal.toLowerCase().includes(word))) {
+//       block2Selector.style.backgroundColor = '#FABD2F';
+//     } else {
+//       block2Selector.style.backgroundColor = '#8EC07C';
+//     }
+//     if (adventurerWords.some((word) => goal.toLowerCase().includes(word))) {
+//       block3Selector.style.backgroundColor = '#FD6D5C';
+//     } else {
+//       block3Selector.style.backgroundColor = '#8EC07C';
+//     }
+//   });
+// }
+
+// Listening for command and changing color of only the first word
+function listenForCommand(elem) {
   elem.addEventListener('input', function () {
     let editorID = this.id;
-    let goalEditor = document.getElementById(editorID);
-    let goal = goalEditor.innerText;
-    console.log(goal);
-    if (artisanWords.some((word) => goal.toLowerCase().includes(word))) {
-      templateExpSelector.style.backgroundColor = '#D3869B';
+    let editor = document.getElementById(editorID);
+    let editorContent = editor.value.toLowerCase();
+    if (editorContent === 'help') {
+      editor.style.color = commands[0].color;
+      commands[0].active = true;
+    } else if (editorContent.includes('achieve')) {
+      editor.style.color = commands[1].color;
+      commands[1].active = true;
+    } else if (editorContent.includes('explore')) {
+      editor.style.color = commands[2].color;
+      commands[2].active = true;
+    } else if (editorContent.includes('innovate')) {
+      editor.style.color = commands[3].color;
+      commands[3].active = true;
+    } else if (editorContent.includes('connect')) {
+      editor.style.color = commands[4].color;
+      commands[4].active = true;
+    } else if (editorContent.includes('fulfill')) {
+      editor.style.color = commands[5].color;
+      commands[5].active = true;
     } else {
-      templateExpSelector.style.backgroundColor = '#8EC07C';
+      editor.style.color = '#ebdbb2';
+      commands.forEach((command) => (command.active = false));
     }
-    if (sentimentalistWords.some((word) => goal.toLowerCase().includes(word))) {
-      block2Selector.style.backgroundColor = '#FABD2F';
-    } else {
-      block2Selector.style.backgroundColor = '#8EC07C';
-    }
-    if (adventurerWords.some((word) => goal.toLowerCase().includes(word))) {
-      block3Selector.style.backgroundColor = '#FD6D5C';
-    } else {
-      block3Selector.style.backgroundColor = '#8EC07C';
-    }
+    return commands;
   });
 }
 
+// Listening for enter key and executing commands
 function listenForEnter(elem) {
   elem.addEventListener('keydown', function (e) {
     let editorID = this.id;
@@ -59,36 +111,55 @@ function listenForEnter(elem) {
     let editorContent = editor.value.toLowerCase();
     if (e.keyCode === 13) {
       e.preventDefault();
-      if (editorContent === 'help') {
+      if (commands[0].active) {
         cliBlockHelp.classList.toggle('hidden');
         cliHelp.focus();
-        cliBlockWelcome.classList.toggle('hidden');
-        selectorGroup.classList.toggle('hidden');
-      } else if (editorContent === 'clear') {
-        cliWelcome.value = '';
-        cliWelcome.focus();
-      } else if (editorContent.includes('achieve')) {
-        commands.achieve = true;
-        cliBlockDemo.classList.toggle('hidden');
-        cliDemo.focus();
-        cliBlockWelcome.classList.toggle('hidden');
-        selectorGroup.classList.toggle('hidden');
+        editor.style.color = '#ebdbb2';
+      } else if (commands[1].active) {
+        prompt.innerText = 'You wrote achieve';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+      } else if (commands[2].active) {
+        prompt.innerText = 'You wrote explore';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+      } else if (commands[3].active) {
+        prompt.innerText = 'You wrote innovate';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+      } else if (commands[4].active) {
+        prompt.innerText = 'You wrote connect';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+      } else if (commands[5].active) {
+        prompt.innerText = 'You wrote fulfill';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+      } else if (editorContent === 'restart') {
+        prompt.innerText =
+          'Namaste. Welcome to Zetsu. What would you like to do?';
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
+        commands.forEach((command) => (command.active = false));
       } else {
-        return;
+        prompt.innerText =
+          "Hmm I don't understand. Write help for a list of commands.";
+        editor.value = '';
+        editor.style.color = '#ebdbb2';
       }
     }
   });
 }
 
-function keepFocus(elem) {
-  elem.addEventListener('blur', function () {
-    this.focus();
-  });
-}
+// function keepFocus(elem) {
+//   elem.addEventListener('blur', function () {
+//     this.focus();
+//   });
+// }
 
-listenForEnter(cliWelcome);
-addInputEventListener(cliWelcome);
-keepFocus(cliWelcome);
+listenForEnter(cliEditor);
+listenForCommand(cliEditor);
+// keepFocus(cliEditor);
 
 // ------------------------------
 
@@ -144,71 +215,59 @@ const adventurerWords = [
   'experience',
 ];
 
-editorTime.addEventListener('input', function () {
-  let timeText = editorTime.innerText;
-  console.log(timeText);
-  if (artisanWords.some((word) => timeText.toLowerCase().includes(word))) {
-    document.getElementById('templateBlock3Selector').style.backgroundColor =
-      'green';
-  } else {
-    document.getElementById('templateBlock3Selector').style.backgroundColor =
-      'red';
-  }
-});
-
 // Path: zetsu-00/public/src/book.js
 
 // ------------------------------
 
-let templateCounter = 0;
+// let templateCounter = 0;
 
-function convertCounter(n) {
-  n = String(templateCounter);
-  if (n.length < 2) {
-    n = '0' + n;
-  }
-  return n;
-}
+// function convertCounter(n) {
+//   n = String(templateCounter);
+//   if (n.length < 2) {
+//     n = '0' + n;
+//   }
+//   return n;
+// }
 
-let parentSection = document.getElementById('content');
-let selectorGroup = document.getElementById('selectorGroup');
+// let parentSection = document.getElementById('content');
+// let selectorGroup = document.getElementById('selectorGroup');
 
-let templateExp = document.getElementById('experienceTemplate');
-let templateExpSelector = document.getElementById('templateExpSelector');
+// let templateExp = document.getElementById('experienceTemplate');
+// let templateExpSelector = document.getElementById('templateExpSelector');
 
-let block2 = document.getElementById('templateBlock2');
-let block2Selector = document.getElementById('templateBlock2Selector');
-let block3 = document.getElementById('templateBlock3');
-let block3Selector = document.getElementById('templateBlock3Selector');
+// let block2 = document.getElementById('templateBlock2');
+// let block2Selector = document.getElementById('templateBlock2Selector');
+// let block3 = document.getElementById('templateBlock3');
+// let block3Selector = document.getElementById('templateBlock3Selector');
 
-templateExpSelector.addEventListener('click', function () {
-  let newTemplate = templateExp.cloneNode(true);
-  let newTemplateHeader = newTemplate.children[0];
-  let newTemplateSubheader = newTemplate.children[1];
-  let newTemplateExample = newTemplateSubheader.children[1];
-  let newID = convertCounter(templateCounter);
-  newTemplate.id = 'experienceTemplate' + newID;
-  newTemplateHeader.id = 'experienceTemplateHeader' + newID;
-  newTemplateExample.id = 'experienceTemplateExample' + newID;
-  parentSection.insertBefore(newTemplate, selectorGroup);
-  newTemplate.classList.toggle('hidden');
-  addInputEventListener(newTemplateExample);
-  hidePlaceholder(newTemplateExample);
-  showPlaceholder(newTemplateExample);
-  revealCommands(newTemplateExample);
-  templateCounter++;
-  selectorGroup.style.display = 'none';
-});
+// templateExpSelector.addEventListener('click', function () {
+//   let newTemplate = templateExp.cloneNode(true);
+//   let newTemplateHeader = newTemplate.children[0];
+//   let newTemplateSubheader = newTemplate.children[1];
+//   let newTemplateExample = newTemplateSubheader.children[1];
+//   let newID = convertCounter(templateCounter);
+//   newTemplate.id = 'experienceTemplate' + newID;
+//   newTemplateHeader.id = 'experienceTemplateHeader' + newID;
+//   newTemplateExample.id = 'experienceTemplateExample' + newID;
+//   parentSection.insertBefore(newTemplate, selectorGroup);
+//   newTemplate.classList.toggle('hidden');
+//   addInputEventListener(newTemplateExample);
+//   hidePlaceholder(newTemplateExample);
+//   showPlaceholder(newTemplateExample);
+//   revealCommands(newTemplateExample);
+//   templateCounter++;
+//   selectorGroup.style.display = 'none';
+// });
 
 // ------------------------------
 
 let viewConsiderations = document.querySelector('.considerations-toggle');
 let considerations = document.getElementById('considerationsPanel');
 
-viewConsiderations.addEventListener('click', function () {
-  considerations.classList.toggle('hidden');
-  console.log('considerations toggled');
-});
+// viewConsiderations.addEventListener('click', function () {
+//   considerations.classList.toggle('hidden');
+//   console.log('considerations toggled');
+// });
 
 // ------------------------------
 
