e3ec20549b5457fd966ff98ecca652915dacaa41
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-04-16 18:20:04 -0400

    Clean up functionality to allow for multiple commands

--- src/app.js
+++ src/app.js
@@ -69,28 +69,11 @@ function listenForCommand(elem) {
     let editorID = this.id;
     let editor = document.getElementById(editorID);
     let editorContent = editor.value.toLowerCase();
-    if (!init && !diagnosis) {
-      if (editorContent === commands[0].name) {
-        editor.style.color = commands[0].color;
-        commands[0].active = true;
-      } else if (editorContent.includes(commands[1].name)) {
-        commands[1].active = true;
-      } else if (editorContent.includes(commands[2].name)) {
-        commands[2].active = true;
-      } else if (editorContent.includes(commands[3].name)) {
-        commands[3].active = true;
-      } else if (editorContent.includes(commands[4].name)) {
-        commands[4].active = true;
-      } else if (editorContent.includes(commands[5].name)) {
-        commands[5].active = true;
-      } else {
-        editor.style.color = '#ebdbb2';
-        commands.forEach((command) => (command.active = false));
-      }
-      return commands;
-    } else {
-      return;
-    }
+    commands.forEach((command) =>
+      editorContent.includes(command.name)
+        ? (command.active = true)
+        : (command.active = false)
+    );
   });
 }
 
@@ -103,73 +86,38 @@ function listenForEnter(elem) {
     let editorContent = editor.value.toLowerCase().trim();
     if (e.keyCode === 13 && editorContent !== '') {
       e.preventDefault();
-      if (init) {
-        user = editor.value.trim();
-        prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> Hello, <span style="color: #83A598">${user}</span>. Please describe your diagnosis.`;
+      command = editorContent;
+      commandOutput = editorValue;
+      let commandsPresent = commands.filter(function (command) {
+        return command.active;
+      });
+      commandsPresent.forEach(
+        (command) =>
+          (commandOutput = commandOutput.replace(
+            command.name,
+            `<span style="color: ${command.color}">${command.name}</span>`
+          ))
+      );
+      if (commands[0].active) {
+        window.open('#cmdModal', '_self');
         editor.value = '';
-        editor.placeholder = 'Cancer type and stage...';
-        editor.style.color = '#ebdbb2';
-        init = false;
-        diagnosis = true;
-      } else if (diagnosis) {
-        disease = editorContent;
-        prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> So <span style="color: #83A598">${user}</span>, what do you want to do with the time you have?`;
-        editor.value = '';
-        editor.placeholder = '"help" to see commands...';
-        editor.style.color = '#ebdbb2';
-        diagnosis = false;
-      } else {
-        command = editorContent;
-        if (commands[0].active) {
-          window.open('#cmdModal', '_self');
-          editor.value = '';
-          editor.style.color = '#ebdbb2';
-        } else if (commands[1].active) {
-          prompt.innerHTML = `<span style="color: #D3869B">&rsaquo; ${editorValue}</span><br><span style="color: #8ec07c">&rsaquo;</span> You wrote ${commands[1].name}`;
-          editor.value = '';
-          editor.placeholder = `${commands[1].name} follow up`;
-          editor.style.color = '#ebdbb2';
-        } else if (commands[2].active) {
-          prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> You wrote ${commands[2].name}`;
-          editor.value = '';
-          editor.placeholder = `${commands[2].name} follow up`;
-          editor.style.color = '#ebdbb2';
-        } else if (commands[3].active) {
-          prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> You wrote ${commands[3].name}`;
-          editor.value = '';
-          editor.placeholder = `${commands[3].name} follow up`;
-          editor.style.color = '#ebdbb2';
-        } else if (commands[4].active) {
-          prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> You wrote ${commands[4].name}`;
-          editor.value = '';
-          editor.placeholder = `${commands[4].name} follow up`;
-          editor.style.color = '#ebdbb2';
-        } else if (commands[5].active) {
-          prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> You wrote ${commands[5].name}`;
-          editor.value = '';
-          editor.placeholder = `${commands[5].name} follow up`;
-          editor.style.color = '#ebdbb2';
-        } else if (editorContent === 'restart') {
-          init = false;
-          diagnosis = false;
-          disease = '';
-          prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> Namaste. Welcome to
+      } else if (editorContent == 'restart') {
+        prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> Namaste. Welcome to
               <span style="color: #8ec07c">Zetsu</span>.
               <br />
               <span style="color: #8ec07c">&rsaquo;</span> With the time you
               have left, what do you want to do?`;
-          editor.style.color = '#ebdbb2';
-          editor.value = '';
-          editor.placeholder = "Start with a command. 'help' for command list.";
-          commands.forEach((command) => (command.active = false));
-        } else {
-          prompt.innerText =
-            "Hmm I don't understand. Write help for a list of commands.";
-          editor.value = '';
-          editor.style.color = '#ebdbb2';
-          command = '';
-        }
+        editor.value = '';
+        commands.forEach((command) => (command.active = false));
+      } else if (commandsPresent.length == 0) {
+        prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> Hmm I'm not following. Did you try using a command?`;
+        editor.value = '';
+      } else {
+        prompt.innerHTML = `<span style="color: #8ec07c">&rsaquo;</span> ${commandOutput}<br><span style="color: #8ec07c">&rsaquo;</span> Here are some things to think about`;
+        editor.value = '';
+        editor.placeholder = `Write a new command. 'help' to see command list.`;
       }
+      return;
     } else {
       return;
     }
