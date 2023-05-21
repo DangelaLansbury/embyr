7b2d793e5a295a7477fee89c95948b80d3eecb4c
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-05-20 23:34:53 -0400

    Clean up variable names and unused functions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
-let cli = document.querySelector('#cli');
+let container = document.querySelector('#container');
+let cliContainer = document.querySelector('#cliContainer');
 let prompt = document.querySelector('#cliPrompt');
-let commandLine = document.querySelector('#commandLine');
-let cliEditor = document.querySelector('#cliEditor');
-let cliInput = document.querySelector('#cliInputText');
+let cli = document.querySelector('#cli');
 let cliSuggest = document.querySelector('#cliSuggest');
-let initExamples = document.querySelector('#initExamples');
 
 // focus on CLI editor at start
 
-cliInput.focus();
+cli.focus();
 
 // --- STATE MANAGEMENT ---
 
@@ -40,9 +38,9 @@ const defaultPrompt = `<div class="cli-prompt-text">Hi there. What would you lik
 const defaultPlaceholder = `Run a command or use -h for help`;
 const commandList = `<div class="cli-prompt-text">Here are some commands you can run:</div>
   <div class="examples">
-    <span style="color: ${commands[0].color}">inn</span> - Access innate immune system tools
+    <span style="color: ${commands[0].color}">innate</span> - Access innate immune system tools
     <br />
-    <span style="color: ${commands[1].color}">ada</span> - Access adaptive immune system tools
+    <span style="color: ${commands[1].color}">adapt</span> - Access adaptive immune system tools
   </div>
   <div class="cli-prompt-text">So, what do you want to do?</div>`;
 const commandExamples = '';
@@ -73,8 +71,8 @@ function returnOutputs() {
     prompt.appendChild(output03);
   }, 900);
   setTimeout(() => {
-    commandLine.style.display = 'flex';
-    cliInput.focus();
+    cliContainer.style.display = 'flex';
+    cli.focus();
   }, 1000);
 }
 
@@ -82,7 +80,7 @@ function returnOutputs() {
 
 // Listening for click and focusing on editor in firefox and safari
 document.body.addEventListener('click', function (e) {
-  cliInput.focus();
+  cli.focus();
 });
 
 // Listening for command and set to true if present
@@ -104,55 +102,6 @@ function listenForBackspace(elem) {
   });
 }
 
-// Listening for input and suggesting commands
-// function listenForInput(elem) {
-//   elem.addEventListener('input', function (e) {
-//     let editorID = this.id;
-//     let editor = document.getElementById(editorID);
-//     let editorValue = editor.innerText;
-//     let editorContent = editorValue.toLowerCase();
-//     if (editorContent.startsWith('a')) {
-//       cliSuggest.innerHTML = `<div class="cli-suggest-text">chieve</div>`;
-//     } else if (editorContent.startsWith('c')) {
-//       cliSuggest.innerHTML = `<div class="cli-suggest-text">onnect</div>`;
-//     } else if (editorContent.startsWith('cont')) {
-//       cliSuggest.innerHTML = `<div class="cli-suggest-text">ribute</div>`;
-//     } else if (editorContent.startsWith('e')) {
-//       cliSuggest.innerHTML = `<div class="cli-suggest-text">xplore</div>`;
-//     } else if (editorContent.startsWith('l')) {
-//       cliSuggest.innerHTML = `<div class="cli-suggest-text">earn</div>`;
-//     } else {
-//       cliSuggest.innerHTML = '';
-//     }
-//   });
-// }
-
-// Listening for tab key and accepting suggestion
-// function listenForTab(elem) {
-//   elem.addEventListener('keydown', function (e) {
-//     let editorID = this.id;
-//     let editor = document.getElementById(editorID);
-//     let editorValue = editor.innerText;
-//     let suggestion = cliSuggest.innerText;
-//     if (e.keyCode === 9 && suggestion !== '') {
-//       e.preventDefault();
-//       editor.innerText = '';
-//       editor.innerText = editorValue + suggestion;
-//       cliSuggest.innerHTML = '';
-//       let editorContent = editor.innerText.toLowerCase();
-//       commands.forEach((command) => (editorContent.includes(command.name) ? (command.active = true) : (command.active = false)));
-//       // Add focus to end of editor after tab
-//       let range = document.createRange();
-//       let sel = window.getSelection();
-//       range.setStart(editor.childNodes[0], editor.innerText.length);
-//       range.collapse(true);
-//       sel.removeAllRanges();
-//       sel.addRange(range);
-//       editor.focus();
-//     }
-//   });
-// }
-
 // Listening for enter key and executing commands
 function listenForEnter(elem) {
   elem.addEventListener('keydown', function (e) {
@@ -163,7 +112,6 @@ function listenForEnter(elem) {
     let editorContent = editorValue.toLowerCase().trim();
     if (e.keyCode === 13 && editorContent !== '') {
       e.preventDefault();
-      // initExamples.classList.add('hidden');
       command = editorContent;
       commandOutput = editorValue;
       console.log(editorID, editorValue, editorContent);
@@ -174,7 +122,7 @@ function listenForEnter(elem) {
       if (editorContent == '-c') {
         prompt.innerHTML = `<div class="cli-user-input">${commandOutput}</div> ${commandList}`;
         editor.innerText = '';
-        cliInput.focus();
+        cli.focus();
       } else if (editorContent == '-h') {
         // trigger link to open modal
         document.querySelector('#stamp').click();
@@ -182,18 +130,18 @@ function listenForEnter(elem) {
       } else if (editorContent == '-e') {
         prompt.innerHTML = `<div class="cli-user-input">${commandOutput}</div> ${commandExamples}`;
         editor.innerText = '';
-        cliInput.focus();
+        cli.focus();
       } else if (editorContent == '-r') {
         prompt.innerHTML = defaultPrompt;
         editor.innerText = '';
         commands.forEach((command) => (command.active = false));
-        cliInput.focus();
+        cli.focus();
       } else if (commandsPresent.length == 0) {
         prompt.innerHTML = nullPrompt;
         editor.innerText = '';
-        cliInput.focus();
+        cli.focus();
       } else {
-        commandLine.style.display = 'none';
+        cliContainer.style.display = 'none';
         prompt.innerHTML = `<div class="cli-user-input">${commandOutput}</div>`;
         output00.innerHTML = `<div class="cli-prompt-text">Immune system respone:</div>`;
         output01.innerHTML = `<div class="cli-prompt-text">Searching for ${commandOutput}...</div>`;
@@ -201,7 +149,7 @@ function listenForEnter(elem) {
         output03.innerHTML = `<div class="cli-prompt-text">And here's what that means for the system.</div>`;
         returnOutputs();
         editor.innerText = '';
-        cliInput.focus();
+        cli.focus();
       }
       return;
     } else {
@@ -211,8 +159,8 @@ function listenForEnter(elem) {
 }
 
 // --- RUNNING FUNCTIONS ---
-listenForCommand(cliInput);
-listenForEnter(cliInput);
-listenForBackspace(cliInput);
+listenForCommand(cli);
+listenForEnter(cli);
+listenForBackspace(cli);
 
 // -h -e -c -r -doc
