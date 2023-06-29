d12135296aaeabf75920ee4fc050e273d6321a82
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-06-29 07:17:14 -0400

    update commands to separate args and modifier

--- src/app.js
+++ src/app.js
@@ -42,15 +42,23 @@ hideCursor(cli);
 const commands = {
   innate: (args) => {
     // code to handle the "innate" command
-    prompt.innerHTML = 'you wrote innate ' + args;
+    // split args into object and modifier where modifier is anything that starts with "--"
+    let modifier = args.filter((arg) => arg.startsWith('--'));
+    let objectArray = args.filter((arg) => !arg.startsWith('--'));
+    let object = objectArray.join(' ');
+    prompt.innerHTML = 'you wrote innate ' + object + ' ' + modifier;
   },
   adapt: (args) => {
     // code to handle the "adapt" command
-    prompt.innerHTML = 'you wrote adapt ' + args;
+    // split args into object and modifier where modifier is anything that starts with "--"
+    let modifier = args.filter((arg) => arg.startsWith('--'));
+    let objectArray = args.filter((arg) => !arg.startsWith('--'));
+    let object = objectArray.join(' ');
+    prompt.innerHTML = 'you wrote adapt ' + object + ' ' + modifier;
   },
-  memory: (args) => {
+  mem: (args) => {
     // code to handle the "memory" command
-    prompt.innerHTML = 'you wrote memory ' + args;
+    prompt.innerHTML = 'you wrote mem ' + args;
   },
   help: () => {
     // code to handle the "help" command
@@ -58,7 +66,7 @@ const commands = {
   },
   clear: () => {
     // code to handle the "clear" command
-    prompt.innerHTML = 'you wrote clear';
+    prompt.innerHTML = 'Hi there. Run a command or use -h for help.';
   },
   new: (args) => {
     // code to handle the "new" command
@@ -117,129 +125,8 @@ cli.addEventListener('keydown', function (e) {
         clearCLI();
       } else {
         prompt.innerHTML = nullPrompt;
+        clearCLI();
       }
     }
   }
 });
-
-// --- Old Code ---
-
-// // commands
-// let command = '';
-// const commands = [
-//   {
-//     name: 'new',
-//     active: false,
-//     color: '#83A598',
-//   },
-//   {
-//     name: 'env',
-//     active: false,
-//     color: '#FD6D5C',
-//   },
-//   {
-//     name: 'status',
-//     active: false,
-//     color: '#FABD2F',
-//   },
-// ];
-
-// // arguments
-// let arg = '';
-// const args = [
-//   {
-//     type: 'new',
-//     arg: ['virus', 'bacteria', 'fungus', 'parasite'],
-//   },
-//   {
-//     type: 'env',
-//     arg: ['radiation', 'stress', 'injury', 'heat', 'cold'],
-//   },
-//   {
-//     type: 'status',
-//     arg: ['innate', 'adaptive'],
-//   },
-// ];
-
-// // modifiers
-// let modifier = '';
-// const modifiers = [
-//   {
-//     type: 'new',
-//     modifier: ['--infectious', '--noninfectious'],
-//   },
-//   {
-//     type: 'env',
-//     modifier: ['--acute', '--chronic'],
-//   },
-// ];
-
-// // Listening for command and set to true if present
-// cli.addEventListener('input', function () {
-//   let editorID = this.id;
-//   let editorContent = document.getElementById(editorID).innerText.toLowerCase();
-//   // remove first word from editor string and check if it matches a command
-//   let editorFirstWord = editorContent.split(' ')[0];
-//   let commandsPresent = commands.filter(function (command) {
-//     return command.name == editorFirstWord;
-//   });
-//   if (commandsPresent.length > 0) {
-//     commandsPresent.forEach((command) => (command.active = true));
-//   } else {
-//     commands.forEach((command) => (command.active = false));
-//   }
-// });
-
-// // --- EXECUTING COMMANDS ---
-
-// // Listening for enter key and executing commands
-// cli.addEventListener('keydown', function (e) {
-//   cliSuggest.innerHTML = '';
-//   let editorID = this.id;
-//   let editor = document.getElementById(editorID);
-//   let editorValue = editor.innerText;
-//   let editorContent = editorValue.toLowerCase().trim();
-//   if (e.keyCode === 13 && editorContent !== '') {
-//     e.preventDefault();
-//     commandOutput = editorContent;
-//     console.log(editorID, editorValue, editorContent);
-//     let commandsPresent = commands.filter(function (command) {
-//       return command.active;
-//     });
-//     commandsPresent.forEach((command) => (commandOutput = commandOutput.replace(command.name, `<span style="color: ${command.color}">${command.name}</span>`)));
-//     if (editorContent == '-c') {
-//       prompt.innerHTML = `<div class="cli-user-input">${commandOutput}</div> ${commandList}`;
-//       clearCLI();
-//     } else if (editorContent == '-h') {
-//       // trigger link to open modal
-//       document.querySelector('#stamp').click();
-//       clearCLI();
-//     } else if (editorContent == '-r') {
-//       prompt.innerHTML = `<div class="cli-prompt-text">Hi there. Run a command or use -h for help.</div>`;
-//       commands.forEach((command) => (command.active = false));
-//       clearCLI();
-//     } else if (commandsPresent.length == 0) {
-//       prompt.innerHTML = nullPrompt;
-//       clearCLI();
-//     } else {
-//       cliContainer.style.display = 'none';
-//       prompt.innerHTML = `<div class="cli-user-input">${commandOutput}</div>`;
-//       output00.innerHTML = `<div class="cli-prompt-text">Immune system respone:</div>`;
-//       output01.innerHTML = `<div class="cli-prompt-text">Searching for ${commandOutput}...</div>`;
-//       output02.innerHTML = `<div class="cli-prompt-text">Here's a relevant piece of info.</div>`;
-//       output03.innerHTML = `<div class="cli-prompt-text">And here's what that means for the system.</div>`;
-//       returnOutput(output00, 300);
-//       returnOutput(output01, 450);
-//       returnOutput(output02, 600);
-//       returnOutput(output03, 900);
-//       setTimeout(() => {
-//         cliContainer.style.display = 'flex';
-//         cli.focus();
-//       }, 1000);
-//       clearCLI();
-//     }
-//     return;
-//   } else {
-//     return;
-//   }
-// });
