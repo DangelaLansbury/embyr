fd5dfa82f7cf130c46ef8a52c7365426db4b926b
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2023-09-26 09:56:24 -0400

    Bring back multiple results list

--- src/app.js
+++ src/app.js
@@ -117,7 +117,6 @@ const populateSuggestion = (command, id) => {
   let cmd = command.split(' ')[0].toLowerCase();
   suggestion.className = 'suggestion thicc sweetgrass';
   suggestion.innerHTML = `<div class="cmd-icon"><img src="public/icons/${cmd}.svg" class="icon-svg" alt="icon for ${cmd}" /></div><div class="suggestion-command" data-id="${id}">${command}</div>`;
-  // suggestion.innerHTML = `<div class="suggestion-command">${command}</div>`;
   suggestionsList.appendChild(suggestion);
 };
 
@@ -260,7 +259,7 @@ zetsu.addEventListener('input', function () {
           }
           let alreadySuggested = false;
           for (let k = 0; k < suggestionsArray.length; k++) {
-            if (suggestionsArray[k].command.toLowerCase() === fullCommand.toLowerCase()) {
+            if (suggestionsArray[k].raw.toLowerCase() === fullCommand.toLowerCase()) {
               alreadySuggested = true;
             }
           }
@@ -273,19 +272,34 @@ zetsu.addEventListener('input', function () {
               similarity: similarity,
             };
             suggestionsArray.push(suggestion);
-            // Sort suggestions by similarity
+            // // Sort suggestions by similarity
+            // if (suggestionsArray.length > 0) {
+            //   suggestionsArray.sort((a, b) => {
+            //     const nameA = a.similarity;
+            //     const nameB = b.similarity;
+            //     return nameB - nameA;
+            //   });
+            // }
+            // // Display top suggestion
+            // suggestionsList.innerHTML = '';
+            // if (!zetsu.innerText.trim().startsWith(suggestionsArray[0].raw)) {
+            //   populateSuggestion(suggestionsArray[0].command, suggestionsArray[0].id);
+            //   displayDetails(commands[command].hints[suggestionsArray[0].id].title, commands[command].hints[suggestionsArray[0].id].description);
+            // }
+            // Reorder suggestions in order of similarity
             if (suggestionsArray.length > 0) {
               suggestionsArray.sort((a, b) => {
                 const nameA = a.similarity;
                 const nameB = b.similarity;
                 return nameB - nameA;
               });
-            }
-            // Display top suggestion
-            suggestionsList.innerHTML = '';
-            if (!zetsu.innerText.trim().startsWith(suggestionsArray[0].raw)) {
-              populateSuggestion(suggestionsArray[0].command, suggestionsArray[0].id);
-              displayDetails(commands[command].hints[suggestionsArray[0].id].title, commands[command].hints[suggestionsArray[0].id].description);
+              // Populate suggestions list with new order
+              suggestionsList.innerHTML = '';
+              for (let k = 0; k < suggestionsArray.length; k++) {
+                if (!zetsu.innerText.startsWith(suggestionsArray[0].raw + ' ')) {
+                  populateSuggestion(suggestionsArray[k].command, suggestionsArray[k].id);
+                }
+              }
             }
           }
         }
@@ -303,120 +317,120 @@ zetsu.addEventListener('input', function () {
   }
   suggestionsArray = [];
   // listen for tab key to use suggestion
-  zetsu.addEventListener('keydown', function (e) {
-    if (e.key === 'Tab') {
-      e.preventDefault();
-      if (suggestions.length !== 0) {
-        let suggestedCommand = suggestions[0].querySelector('.suggestion-command').innerText;
-        // If suggestion is only one word, add space after
-        if (suggestedCommand.split(' ').length === 1) {
-          suggestedCommand = suggestedCommand + ' ';
-        }
-        zetsu.innerText = suggestedCommand;
-        focusAtEnd();
-        suggestions = [];
-        showZetsuInit();
-      }
-    }
-  });
-  let suggestionIndex = -1;
-  // // Listening for up and down arrow keys to cycle through suggestions
   // zetsu.addEventListener('keydown', function (e) {
-  //   for (let i = 0; i < suggestions.length; i++) {
-  //     suggestions[i].classList.remove('active');
-  //   }
-  //   // Check if suggestions are present
-  //   if (suggestions.length !== 0) {
-  //     if (e.key === 'ArrowDown') {
-  //       e.preventDefault();
-  //       details.innerHTML = '';
-  //       if (cursor.style.display === 'inline-flex') {
-  //         cursor.style.display = 'none';
+  //   if (e.key === 'Tab') {
+  //     e.preventDefault();
+  //     if (suggestions.length !== 0) {
+  //       let suggestedCommand = suggestions[0].querySelector('.suggestion-command').innerText;
+  //       // If suggestion is only one word, add space after
+  //       if (suggestedCommand.split(' ').length === 1) {
+  //         suggestedCommand = suggestedCommand + ' ';
   //       }
-  //       if (suggestionIndex !== -1) {
-  //         suggestions[suggestionIndex].classList.remove('active');
-  //       }
-  //       suggestionIndex++;
-  //       if (suggestionIndex > suggestions.length - 1) {
-  //         suggestionIndex = -1;
-  //         if (input !== '') {
-  //           zetsu.innerText = input;
-  //         } else {
-  //           zetsu.innerText = '';
-  //         }
-  //       } else if (suggestionIndex !== -1) {
-  //         suggestions[suggestionIndex].classList.add('active');
-  //         let suggestedCommand = suggestions[suggestionIndex].querySelector('.suggestion-command').innerText;
-  //         let suggestedCommandID = suggestions[suggestionIndex].querySelector('.suggestion-command').dataset.id;
-  //         let title = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].title;
-  //         let description = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].description;
-  //         zetsu.innerText = suggestedCommand;
-  //         // Display suggestion details
-  //         if (suggestions.length !== 0 && suggestionIndex !== -1) {
-  //           displayDetails(title, description);
-  //         } else {
-  //           details.innerHTML = '';
-  //         }
-  //         suggestions[suggestionIndex].scrollIntoView({
-  //           block: 'nearest',
-  //           inline: 'end',
-  //           behavior: 'smooth',
-  //         });
-  //       }
-  //       if (zetsu.innerText !== '') {
-  //         focusAtEnd();
-  //       } else {
-  //         cursor.style.display = 'inline-flex';
-  //       }
-  //     } else if (e.key === 'ArrowUp') {
-  //       e.preventDefault();
-  //       details.innerHTML = '';
-  //       if (cursor.style.display === 'inline-flex') {
-  //         cursor.style.display = 'none';
-  //       }
-  //       if (suggestionIndex !== -1) {
-  //         suggestions[suggestionIndex].classList.remove('active');
-  //       }
-  //       suggestionIndex--;
-  //       if (suggestionIndex === -1) {
-  //         if (input !== '') {
-  //           zetsu.innerText = input;
-  //         } else {
-  //           zetsu.innerText = '';
-  //         }
-  //       }
-  //       if (suggestionIndex < -1) {
-  //         suggestionIndex = suggestions.length - 1;
-  //       }
-  //       if (suggestionIndex !== -1) {
-  //         suggestions[suggestionIndex].classList.add('active');
-  //         let suggestedCommand = suggestions[suggestionIndex].querySelector('.suggestion-command').innerText;
-  //         let suggestedCommandID = suggestions[suggestionIndex].querySelector('.suggestion-command').dataset.id;
-  //         let title = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].title;
-  //         let description = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].description;
-  //         zetsu.innerText = suggestedCommand;
-  //         // Display suggestion details
-  //         if (suggestions.length !== 0 && suggestionIndex !== -1) {
-  //           displayDetails(title, description);
-  //         } else {
-  //           details.innerHTML = '';
-  //         }
-  //         suggestions[suggestionIndex].scrollIntoView({
-  //           block: 'nearest',
-  //           inline: 'end',
-  //           behavior: 'smooth',
-  //         });
-  //       }
-  //       if (zetsu.innerText !== '') {
-  //         focusAtEnd();
-  //       } else {
-  //         cursor.style.display = 'inline-flex';
-  //       }
+  //       zetsu.innerText = suggestedCommand;
+  //       focusAtEnd();
+  //       suggestions = [];
+  //       showZetsuInit();
   //     }
-  //   } else {
-  //     suggestionIndex = -1;
   //   }
   // });
+  let suggestionIndex = -1;
+  // Listening for up and down arrow keys to cycle through suggestions
+  zetsu.addEventListener('keydown', function (e) {
+    for (let i = 0; i < suggestions.length; i++) {
+      suggestions[i].classList.remove('active');
+    }
+    // Check if suggestions are present
+    if (suggestions.length !== 0) {
+      if (e.key === 'ArrowDown') {
+        e.preventDefault();
+        details.innerHTML = '';
+        if (cursor.style.display === 'inline-flex') {
+          cursor.style.display = 'none';
+        }
+        if (suggestionIndex !== -1) {
+          suggestions[suggestionIndex].classList.remove('active');
+        }
+        suggestionIndex++;
+        if (suggestionIndex > suggestions.length - 1) {
+          suggestionIndex = -1;
+          if (input !== '') {
+            zetsu.innerText = input;
+          } else {
+            zetsu.innerText = '';
+          }
+        } else if (suggestionIndex !== -1) {
+          suggestions[suggestionIndex].classList.add('active');
+          let suggestedCommand = suggestions[suggestionIndex].querySelector('.suggestion-command').innerText;
+          let suggestedCommandID = suggestions[suggestionIndex].querySelector('.suggestion-command').dataset.id;
+          let title = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].title;
+          let description = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].description;
+          zetsu.innerText = suggestedCommand;
+          // Display suggestion details
+          if (suggestions.length !== 0 && suggestionIndex !== -1) {
+            displayDetails(title, description);
+          } else {
+            details.innerHTML = '';
+          }
+          suggestions[suggestionIndex].scrollIntoView({
+            block: 'nearest',
+            inline: 'end',
+            behavior: 'smooth',
+          });
+        }
+        if (zetsu.innerText !== '') {
+          focusAtEnd();
+        } else {
+          cursor.style.display = 'inline-flex';
+        }
+      } else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        details.innerHTML = '';
+        if (cursor.style.display === 'inline-flex') {
+          cursor.style.display = 'none';
+        }
+        if (suggestionIndex !== -1) {
+          suggestions[suggestionIndex].classList.remove('active');
+        }
+        suggestionIndex--;
+        if (suggestionIndex === -1) {
+          if (input !== '') {
+            zetsu.innerText = input;
+          } else {
+            zetsu.innerText = '';
+          }
+        }
+        if (suggestionIndex < -1) {
+          suggestionIndex = suggestions.length - 1;
+        }
+        if (suggestionIndex !== -1) {
+          suggestions[suggestionIndex].classList.add('active');
+          let suggestedCommand = suggestions[suggestionIndex].querySelector('.suggestion-command').innerText;
+          let suggestedCommandID = suggestions[suggestionIndex].querySelector('.suggestion-command').dataset.id;
+          let title = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].title;
+          let description = commands[suggestedCommand.split(' ')[0]].hints[suggestedCommandID].description;
+          zetsu.innerText = suggestedCommand;
+          // Display suggestion details
+          if (suggestions.length !== 0 && suggestionIndex !== -1) {
+            displayDetails(title, description);
+          } else {
+            details.innerHTML = '';
+          }
+          suggestions[suggestionIndex].scrollIntoView({
+            block: 'nearest',
+            inline: 'end',
+            behavior: 'smooth',
+          });
+        }
+        if (zetsu.innerText !== '') {
+          focusAtEnd();
+        } else {
+          cursor.style.display = 'inline-flex';
+        }
+      }
+    } else {
+      suggestionIndex = -1;
+    }
+  });
   // Reset suggestion index when user presses space or continues to type or hits enter
   zetsu.addEventListener('keydown', function (e) {
     if (e.key === ' ' || e.key === 'Backspace' || e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
