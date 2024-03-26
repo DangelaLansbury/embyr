09a56b4df596ebb16257441c87d00e19086fd619
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2024-03-26 00:12:47 -0400

    Update inline-command styling

--- src/OS.js
+++ src/OS.js
@@ -165,21 +165,21 @@ const commands = {
     name: 'git',
     keywords: [...gitKeywords, ...newKeywords, ...cellKeywords],
     do: `git`,
-    description: `The <span class="lilac thicc">git</span> command is used to track and manage changes in the embryonic stem cell's lineage and form.`,
-    preview: `The <span class="lilac thicc">git</span> command is used to track and manage changes in the embryonic stem cell's lineage and form.`,
+    description: `The <span class="lilac thicc"> git </span> command is used to track and manage changes in the embryonic stem cell's lineage and form.`,
+    preview: `The <span class="lilac thicc inline-command"> git </span> command is used to track and manage changes in the embryonic stem cell's lineage and form.`,
     subCommands: {
       new: {
         name: 'new',
         keywords: [...gitKeywords, ...newKeywords, ...cellKeywords],
         do: `git new`,
-        description: `The <span class="lilac thicc">new</span> command is used to create a new cell, tissue, organ, or organism.`,
-        preview: `The <span class="lilac thicc">new</span> command is used to create a new cell, tissue, organ, or organism.`,
+        description: `The <span class="lilac thicc"> new </span> command is used to create a new cell, tissue, organ, or organism.`,
+        preview: `The <span class="lilac thicc inline-command">new</span> command is used to create a new cell, tissue, organ, or organism.`,
         ops: {
           cell: {
             keywords: [...cellKeywords],
             do: 'git new cell',
             description: `The <span class="lilac thicc">cell</span> command is used to create a new cell.`,
-            preview: `The <span class="lilac thicc">cell</span> command is used to create a new cell.`,
+            preview: `The <span class="lilac thicc inline-command">cell</span> command is used to create a new cell.`,
             run: (inputArray) => {
               runMakeTissue(inputArray);
             },
